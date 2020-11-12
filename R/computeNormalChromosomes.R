aaeabc0fa5871a49de768c19169a5c8246a5ab54
gaia-mazzocchetti <gaia.mazzocchetti@studio.unibo.it>
2020-11-12 12:32:54 +0100

    added minimum and maximum threshold to filter segments with normal CN

--- R/computeNormalChromosomes.R
+++ R/computeNormalChromosomes.R
@@ -2,6 +2,9 @@
 #'
 #' @param segments data.frame formatted with correct column names
 #' @param perc decimal value of alteration frequency, by default is 0.15
+#' @param maxCN threshold of max copy number to consider, by default is 6
+#' @param min_threshold minimum threshold to define a normal CN, by default is 1.60
+#' @param max_threshold maximum threshold to define a normal CN, by default is 2.40
 #'
 #' @importFrom dplyr filter group_by summarise arrange
 #' @importFrom tidyr %>%
@@ -17,24 +20,24 @@
 #' }
 
 
-computeNormalChromosomes <- function(segments, perc = 0.15) {
+computeNormalChromosomes <- function(segments, perc = 0.15, maxCN= 6, min_threshold= 1.60, max_threshold=2.40) {
 
   ID <- arm <- weighted.mean <- CN <- width <- weighted_mean_CN <- NULL
   samples <- segments$ID%>% unique()
+  segments$CN[segments$CN > 6] <- maxCN
 
   segments$width <- segments$end - segments$start
 
   all_chromosome <- data.frame()
 
-  i=1
   for (i in 1:length(samples)) {
-
+    print(paste0( i," - ",samples[i]))
     segments_sample <- segments %>% filter(ID == samples[i])
 
     CN_CHR <- segments_sample %>%
       group_by(arm) %>%
       summarise(weighted_mean_CN = weighted.mean(CN, w = width),.groups = 'drop_last') %>%
-      filter(weighted_mean_CN <= 2.10 & weighted_mean_CN >=1.90)
+      filter(weighted_mean_CN <= max_threshold & weighted_mean_CN >= min_threshold)
 
     all_chromosome <- rbind(all_chromosome, CN_CHR)
 
