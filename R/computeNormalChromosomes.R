1c969182a89a391fc5902e3132890485ee07cebd
gaia-mazzocchetti <67413031+gaia-mazzocchetti@users.noreply.github.com>
2021-10-20 15:45:34 +0200

    ComputeNormalChromosome update

--- R/computeNormalChromosomes.R
+++ R/computeNormalChromosomes.R
@@ -3,7 +3,7 @@
 #' @description This function compute the DRrefits' input "chromosome list". It is a vector that contains the chromosomal arms considered "normal" in the cohort of samples tested (BED file), under a specific tolerance value
 #'
 #' @param segments data.frame formatted with correct column names
-#' @param tollerance_val decimal value of alteration frequency, by default is 0.15
+#' @param tolerance_val decimal value of alteration frequency, by default is 0.15
 #' @param maxCN threshold of max copy number to consider, by default is 6
 #' @param min_threshold minimum threshold to define a normal CN, by default is 1.60
 #' @param max_threshold maximum threshold to define a normal CN, by default is 2.40
@@ -26,41 +26,46 @@
 
 computeNormalChromosomes <- function(segments, tolerance_val = 0.15, maxCN= 6, min_threshold= 1.60, max_threshold=2.40) {
 
-  ID <- arm <- weighted.mean <- CN <- width <- weighted_mean_CN <- alteration_rate <- NULL
+  
+  ID <- chrarm <- weighted.mean <- CN <- width <- weighted_mean_CN <- normal_frequency <- NULL
   samples <- segments$ID%>% unique()
   segments$CN[segments$CN > 6] <- maxCN
-
+  
+  segments$width <- segments$end - segments$start
+  
   all_chromosome <- data.frame()
-
-  for (i in seq_along(samples)) {
-    message(i," - ",samples[i])
+  
+  i=1
+  for (i in 1:length(samples)) {
+    print(paste0( i," - ",samples[i]))
     segments_sample <- segments %>% filter(ID == samples[i])
-
+    
     CN_CHR <- segments_sample %>%
-      group_by(arm) %>%
+      group_by(chrarm) %>%
       summarise(weighted_mean_CN = weighted.mean(CN, w = width),.groups = 'drop_last') %>%
       filter(weighted_mean_CN <= max_threshold & weighted_mean_CN >= min_threshold)
-
+    
     all_chromosome <- rbind(all_chromosome, CN_CHR)
-
+    
   }
-
-  result <- (table(all_chromosome$arm)/length(samples) )
+  
+  result <- (table(all_chromosome$chrarm)/length(samples) )
   result_filtered <- result[result > 1 - tolerance_val]
-
-
-
-  df <- data.frame(arm=names(result), alteration_rate= as.numeric(1- result), observations= table(all_chromosome$arm))
-  df$arm <- df$arm %>% factor(levels = str_sort(df$arm, numeric = TRUE))
+  
+  
+  
+  df <- data.frame(chrarm=names(result), alteration_rate= as.numeric(1- result), observations= table(all_chromosome$chrarm))
+  df$chrarm <- df$chrarm %>% factor(levels = str_sort(df$chrarm, numeric = TRUE))
   print(
-    ggplot(df, aes(x=arm, y=alteration_rate)) +
+    ggplot(df, aes(x=chrarm, y=alteration_rate)) +
       geom_bar(stat = "identity", aes( fill= alteration_rate > tolerance_val)) +
       geom_hline(yintercept = tolerance_val, colour= "black", linetype= 2)+
       geom_text( aes(label= round(alteration_rate, 2), vjust = 1.4))+
-      scale_fill_manual(values = c("#1E90FF","#FF4040")) + 
-      theme(legend.position="bottom")
+      scale_fill_manual(values = c("#1E90FF","#FF4040"))+
+      labs(y="Alteration Rate (%)", x = "Chromosomal arm")
+    
   )
-
+  
   names(result_filtered)
-
+  
 }
