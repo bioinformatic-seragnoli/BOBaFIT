7ff9e59fa99424165a6dec14c269920f09b35ef4
gaia-mazzocchetti <67413031+gaia-mazzocchetti@users.noreply.github.com>
2021-11-17 16:33:34 +0100

    verbose argumet in DRrefit and computeNormalChromosome

--- R/DRrefit.R
+++ R/DRrefit.R
@@ -6,6 +6,7 @@
 #' @param chrlist list of normal chromosome arms (pathology-specific)
 #' @param maxCN threshold of max copy number to consider. By default is 6
 #' @param clust_method clustering method. By default is "ward.D2"
+#' @param verbose print information about the processes of the function. By default is FALSE
 
 #' @return Return two data frames, one is the DRrefit-corrected segments and the other is the samples report. See the vignette for data frame descriptions.
 #' @export
@@ -31,7 +32,8 @@
 DRrefit <- function(segments_chort,
                     chrlist,
                     maxCN = 6,
-                    clust_method = "ward.D2") {
+                    clust_method = "ward.D2",
+                    verbose= FALSE) {
   
   ward.D2 <- ID <- chrarm <- CN <- width <- chr <- cluster <- CN_corrected <- number <- NULL
   
@@ -45,102 +47,203 @@ DRrefit <- function(segments_chort,
   
   samples <- segments_chort$ID %>% unique()
   
-  
-  for (i in seq_along(samples)){
+  if (verbose == TRUE) {
     
-    cat("sample n: ",i," - ",samples[i],"\n")
-    
-    segments <- segments_chort %>%  filter(ID==samples[i])
-    
-    CN_CHR <- segments %>%
-      group_by(chrarm) %>%
-      summarise(weighted_mean_CN = weighted.mean(CN, w = width), .groups = 'drop')
-    
-    
-    CN_CHR_values <- CN_CHR$weighted_mean_CN
-    
-    
-    TRY <- try({
+    for (i in seq_along(samples)){
+      
+      cat("sample n: ",i," - ",samples[i],"\n")
+      
+      segments <- segments_chort %>%  filter(ID==samples[i])
+      
+      CN_CHR <- segments %>%
+        group_by(chrarm) %>%
+        summarise(weighted_mean_CN = weighted.mean(CN, w = width), .groups = 'drop')
       
       
-      capture.output(ClustRes <- NbClust(CN_CHR_values, distance = "euclidean", method = clust_method, index = "all", min.nc = 2, max.nc = 6))
+      CN_CHR_values <- CN_CHR$weighted_mean_CN
       
       
-      CLUST_TABLE <- data.frame(chr=CN_CHR$chrarm, cluster=ClustRes$Best.partition, stringsAsFactors = FALSE)
+      TRY <- try({
+        
+        
+        capture.output(ClustRes <- NbClust(CN_CHR_values, distance = "euclidean", method = clust_method, index = "all", min.nc = 2, max.nc = 6),  type = c("output", "message"))
+        
+        
+        CLUST_TABLE <- data.frame(chr=CN_CHR$chrarm, cluster=ClustRes$Best.partition, stringsAsFactors = FALSE)
+        
+        
+        CLUST_TABLE_in_list_sorted <- CLUST_TABLE %>%
+          filter(chr %in% chrlist ) %>%
+          group_by(cluster) %>%
+          summarise(number = n()) %>%
+          arrange(desc(number))
+        
+        
+        ref_cluster <- CLUST_TABLE_in_list_sorted$cluster[1]
+        
+        
+        
+        ref_cluster <- CLUST_TABLE_in_list_sorted$cluster[1]
+        
+        
+        ref_cluster_chrs <- CLUST_TABLE %>% filter(cluster==ref_cluster)
+        chrlist_cluster <- ref_cluster_chrs$chr
+        
+      }, silent = TRUE)
+      
+      if (is(TRY, "try-error")){
+        message("Clustering failed")
+        
+        new_chrlist <-  chrlist
+        
+        samp_report <-data.frame(
+          sample=samples[i],
+          clustering="FAIL",
+          ref_clust_chr=paste(chrlist, collapse = ", "),
+          num_clust=NA)
+        
+        
+        
+        
+      } else {
+        message("Clustering succeded")
+        
+        new_chrlist <-  chrlist_cluster
+        
+        samp_report <-data.frame(
+          sample=samples[i],
+          clustering="SUCCEDED",
+          ref_clust_chr=paste(chrlist_cluster, collapse = ", "),
+          num_clust=max(ClustRes$Best.partition))
+        
+        
+      }
       
       
-      CLUST_TABLE_in_list_sorted <- CLUST_TABLE %>%
-        filter(chr %in% chrlist ) %>%
-        group_by(cluster) %>%
-        summarise(number = n()) %>%
-        arrange(desc(number))
+      segments_REF <- segments %>% filter(chrarm %in% new_chrlist)
       
+      segments_REF <- segments %>% filter(chrarm %in% new_chrlist)
       
-      ref_cluster <- CLUST_TABLE_in_list_sorted$cluster[1]
+      CN_CHR_REF <- segments_REF %>%
+        group_by(chrarm) %>%
+        summarise(weighted_mean_CN = weighted.mean(CN, w = width), .groups = 'drop')
       
+      real_diploid_region <-  median(CN_CHR_REF$weighted_mean_CN)
       
+      correction_factor <- 2 - real_diploid_region
       
-      ref_cluster <- CLUST_TABLE_in_list_sorted$cluster[1]
+      samp_report$correction_factor <- correction_factor
       
+      samp_report$correction_class <- ifelse(correction_factor > 0.5, "REFITTED",
+                                             ifelse(correction_factor <= 0.1, "NO CHANGES", "RECALIBRATED"))
       
-      ref_cluster_chrs <- CLUST_TABLE %>% filter(cluster==ref_cluster)
-      chrlist_cluster <- ref_cluster_chrs$chr
+      report_clustering_list [[i]]<- samp_report
       
-    })
-    
-    if (is(TRY, "try-error")){
-      message("Clustering failed")
+      segments$CN_corrected <- segments$CN + correction_factor
       
-      new_chrlist <-  chrlist
+      segments$CN_corrected <- ifelse(segments$CN_corrected < 0, 0.001, segments$CN_corrected)
       
-      samp_report <-data.frame(
-        sample=samples[i],
-        clustering="FAIL",
-        ref_clust_chr=paste(chrlist, collapse = ", "),
-        num_clust=NA)
-      
-      
-      
-      
-    } else {
-      message("Clustering succeded")
-      
-      new_chrlist <-  chrlist_cluster
-      
-      samp_report <-data.frame(
-        sample=samples[i],
-        clustering="SUCCEDED",
-        ref_clust_chr=paste(chrlist_cluster, collapse = ", "),
-        num_clust=max(ClustRes$Best.partition))
-      
+      segments_chort_corrected_list[[i]] <- segments
       
     }
     
+  } else {
     
-    segments_REF <- segments %>% filter(chrarm %in% new_chrlist)
-    
-    segments_REF <- segments %>% filter(chrarm %in% new_chrlist)
-    
-    CN_CHR_REF <- segments_REF %>%
-      group_by(chrarm) %>%
-      summarise(weighted_mean_CN = weighted.mean(CN, w = width), .groups = 'drop')
-    
-    real_diploid_region <-  median(CN_CHR_REF$weighted_mean_CN)
-    
-    correction_factor <- 2 - real_diploid_region
-    
-    samp_report$correction_factor <- correction_factor
-    
-    samp_report$correction_class <- ifelse(correction_factor > 0.5, "REFITTED",
-                                           ifelse(correction_factor <= 0.1, "NO CHANGES", "RECALIBRATED"))
-    
-    report_clustering_list [[i]]<- samp_report
-    
-    segments$CN_corrected <- segments$CN + correction_factor
-    
-    segments$CN_corrected <- ifelse(segments$CN_corrected < 0, 0.001, segments$CN_corrected)
-    
-    segments_chort_corrected_list[[i]] <- segments
+    for (i in seq_along(samples)){
+      
+      
+      segments <- segments_chort %>%  filter(ID==samples[i])
+      
+      CN_CHR <- segments %>%
+        group_by(chrarm) %>%
+        summarise(weighted_mean_CN = weighted.mean(CN, w = width), .groups = 'drop')
+      
+      
+      CN_CHR_values <- CN_CHR$weighted_mean_CN
+      
+      
+      TRY <- try({
+        
+        
+        capture.output(ClustRes <- NbClust(CN_CHR_values, distance = "euclidean", method = clust_method, index = "all", min.nc = 2, max.nc = 6), type =c("output", "message"))
+        
+        
+        CLUST_TABLE <- data.frame(chr=CN_CHR$chrarm, cluster=ClustRes$Best.partition, stringsAsFactors = FALSE)
+        
+        
+        CLUST_TABLE_in_list_sorted <- CLUST_TABLE %>%
+          filter(chr %in% chrlist ) %>%
+          group_by(cluster) %>%
+          summarise(number = n()) %>%
+          arrange(desc(number))
+        
+        
+        ref_cluster <- CLUST_TABLE_in_list_sorted$cluster[1]
+        
+        
+        
+        ref_cluster <- CLUST_TABLE_in_list_sorted$cluster[1]
+        
+        
+        ref_cluster_chrs <- CLUST_TABLE %>% filter(cluster==ref_cluster)
+        chrlist_cluster <- ref_cluster_chrs$chr
+        
+      }, silent = TRUE)
+      
+      if (is(TRY, "try-error")){
+        
+        
+        new_chrlist <-  chrlist
+        
+        samp_report <-data.frame(
+          sample=samples[i],
+          clustering="FAIL",
+          ref_clust_chr=paste(chrlist, collapse = ", "),
+          num_clust=NA)
+        
+        
+        
+        
+      } else {
+        
+        new_chrlist <-  chrlist_cluster
+        
+        samp_report <-data.frame(
+          sample=samples[i],
+          clustering="SUCCEDED",
+          ref_clust_chr=paste(chrlist_cluster, collapse = ", "),
+          num_clust=max(ClustRes$Best.partition))
+        
+        
+      }
+      
+      
+      segments_REF <- segments %>% filter(chrarm %in% new_chrlist)
+      
+      segments_REF <- segments %>% filter(chrarm %in% new_chrlist)
+      
+      CN_CHR_REF <- segments_REF %>%
+        group_by(chrarm) %>%
+        summarise(weighted_mean_CN = weighted.mean(CN, w = width), .groups = 'drop')
+      
+      real_diploid_region <-  median(CN_CHR_REF$weighted_mean_CN)
+      
+      correction_factor <- 2 - real_diploid_region
+      
+      samp_report$correction_factor <- correction_factor
+      
+      samp_report$correction_class <- ifelse(correction_factor > 0.5, "REFITTED",
+                                             ifelse(correction_factor <= 0.1, "NO CHANGES", "RECALIBRATED"))
+      
+      report_clustering_list [[i]]<- samp_report
+      
+      segments$CN_corrected <- segments$CN + correction_factor
+      
+      segments$CN_corrected <- ifelse(segments$CN_corrected < 0, 0.001, segments$CN_corrected)
+      
+      segments_chort_corrected_list[[i]] <- segments
+      
+    }
     
   }
   
@@ -152,4 +255,3 @@ DRrefit <- function(segments_chort,
   
   OUTPUT
 }
-
--- R/computeNormalChromosomes.R
+++ R/computeNormalChromosomes.R
@@ -3,10 +3,11 @@
 #' @description This function compute the DRrefits' input "chromosome list". It is a vector that contains the chromosomal arms considered "normal" in the cohort of samples tested (BED file), under a specific tolerance value
 #'
 #' @param segments data.frame formatted with correct column names
-#' @param tolerance_val decimal value of alteration frequency, by default is 0.15
-#' @param maxCN threshold of max copy number to consider, by default is 6
-#' @param min_threshold minimum threshold to define a normal CN, by default is 1.60
-#' @param max_threshold maximum threshold to define a normal CN, by default is 2.40
+#' @param tolerance_val decimal value of alteration frequency. By default is 0.15
+#' @param maxCN threshold of max copy number to consider. By default is 6
+#' @param min_threshold minimum threshold to define a normal CN. By default is 1.60
+#' @param max_threshold maximum threshold to define a normal CN. By default is 2.40
+#' @param verbose print information about the processes of the function. By default is FALSE
 #'
 #' @importFrom dplyr filter group_by summarise arrange
 #' @importFrom tidyr %>%
@@ -24,22 +25,24 @@
 
 
 
-computeNormalChromosomes <-
-  function(segments,
-           tolerance_val = 0.15,
-           maxCN = 6,
-           min_threshold = 1.60,
-           max_threshold = 2.40) {
-    ID <-
-      arm <-
-      chrarm <-
-      weighted.mean <-
-      CN <- width <- weighted_mean_CN <- alteration_rate <- NULL
-    samples <- segments$ID %>% unique()
-    segments$CN[segments$CN > 6] <- maxCN
-    
-    all_chromosome <- data.frame()
-    
+computeNormalChromosomes <- function(segments,
+                                     tolerance_val = 0.15,
+                                     maxCN = 6,
+                                     min_threshold = 1.60,
+                                     max_threshold = 2.40,
+                                     verbose = FALSE) {
+  ID <-
+    arm <-
+    chrarm <-
+    weighted.mean <-
+    CN <- width <- weighted_mean_CN <- alteration_rate <- NULL
+  
+  samples <- segments$ID %>% unique()
+  segments$CN[segments$CN > 6] <- maxCN
+  
+  all_chromosome <- data.frame()
+  
+  if (verbose == TRUE) {  
     for (i in seq_along(samples)) {
       message(i, " - ", samples[i])
       segments_sample <- segments %>% filter(ID == samples[i])
@@ -54,36 +57,50 @@ computeNormalChromosomes <-
       all_chromosome <- rbind(all_chromosome, CN_CHR)
       
     }
-    
-    result <- (table(all_chromosome$chrarm) / length(samples))
-    result_filtered <- result[result > 1 - tolerance_val]
-    
-    
-    
-    df <-
-      data.frame(
-        arm = names(result),
-        alteration_rate = as.numeric(1 - result),
-        observations = table(all_chromosome$chrarm)
-      )
-    df$arm <-
-      df$arm %>% factor(levels = str_sort(df$arm, numeric = TRUE))
-    print(
-      ggplot(df, aes(x = arm, y = alteration_rate)) +
-        geom_bar(stat = "identity", aes(fill = alteration_rate > tolerance_val)) +
-        geom_hline(
-          yintercept = tolerance_val,
-          colour = "black",
-          linetype = 2
-        ) +
-        geom_text(aes(
-          label = round(alteration_rate, 2), vjust = 1.4
-        )) +
-        scale_fill_manual(values = c("#1E90FF", "#FF4040")) +
-        theme(legend.position = "bottom") +
-        labs(y = "Alteration Rate (%)", x = "Chromosomal arm")
-    )
-    
-    names(result_filtered)
-    
+  } else {
+    for (i in seq_along(samples)) {
+      segments_sample <- segments %>% filter(ID == samples[i])
+      
+      CN_CHR <- segments_sample %>%
+        group_by(chrarm) %>%
+        summarise(weighted_mean_CN = weighted.mean(CN, w = width),
+                  .groups = 'drop_last') %>%
+        filter(weighted_mean_CN <= max_threshold &
+                 weighted_mean_CN >= min_threshold)
+      
+      all_chromosome <- rbind(all_chromosome, CN_CHR)
+    }
   }
+  
+  result <- (table(all_chromosome$chrarm) / length(samples))
+  result_filtered <- result[result > 1 - tolerance_val]
+  
+  
+  
+  df <-
+    data.frame(
+      arm = names(result),
+      alteration_rate = as.numeric(1 - result),
+      observations = table(all_chromosome$chrarm)
+    )
+  df$arm <-
+    df$arm %>% factor(levels = str_sort(df$arm, numeric = TRUE))
+  print(
+    ggplot(df, aes(x = arm, y = alteration_rate)) +
+      geom_bar(stat = "identity", aes(fill = alteration_rate > tolerance_val)) +
+      geom_hline(
+        yintercept = tolerance_val,
+        colour = "black",
+        linetype = 2
+      ) +
+      geom_text(aes(
+        label = round(alteration_rate, 2), vjust = 1.4
+      )) +
+      scale_fill_manual(values = c("#1E90FF", "#FF4040")) +
+      theme(legend.position = "bottom") +
+      labs(y = "Alteration Rate (%)", x = "Chromosomal arm")
+  )
+  
+  names(result_filtered)
+  
+}
